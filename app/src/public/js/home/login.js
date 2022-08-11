baacab9f750e2926af819d624e76934777918441
Terry <terry@Terryui-MacBookPro.local>
2022-08-11 14:44:03 +0800

    dom used

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,15 @@
 "use strict";
 
-console.log("hello");
-console.log("bye");
+const id = document.querySelector("#id"),
+    pw = document.querySelector("#pw"),
+    loginButton = document.querySelector("button");
+
+loginButton.addEventListener("click", login);
+
+function login() {
+    const req = {
+        id: id.value,
+        pw: pw.value,
+    };
+    console.log(req);
+}
