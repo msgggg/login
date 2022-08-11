5d315db1df05bebf0c93448e9146063bf90bc7aa
Terry <terry@Terryui-MacBookPro.local>
2022-08-11 15:08:17 +0800

    login seen

--- app/app.js
+++ app/app.js
@@ -2,7 +2,7 @@
 
 // module
 const express = require("express");
-const { use } = require("./src/routes/home");
+const bodyParser = require("body-parser");
 const app = express();
 
 // routing
@@ -12,6 +12,9 @@ const home = require("./src/routes/home");
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
 app.use(express.static(`${__dirname}/src/public`))
+app.use(bodyParser.json());
+//URL을 통해 전달되는 데이터에 한글, 공백과 같은 문자가 포함될 경우 제대로 인식되지 않는 문제 해결
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/", home); // use -> 미들웨어를 등록해주는 method
 
